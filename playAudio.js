cdfd4e9d27cd0d924e9bbab86160c97b2bc9e88a
roger <1664465219@qq.com>
2022-01-10 22:56:05 +0800

    添加背景音乐

--- playAudio.js
+++ playAudio.js
@@ -1,8 +1,11 @@
 var myAudioElement = document.getElementById('audio');  
-
+var isPlay = false;
 myAudioElement.addEventListener("canplaythrough", event => {
   document.body.addEventListener("click", event => {
-    /* 音频可以播放；如果权限允许则播放 */
-    myAudioElement.play();
+    if (!isPlay) {
+      /* 音频可以播放；如果权限允许则播放 */
+      myAudioElement.play();
+      isPlay = true;
+    }
   });
 });
