f4353e362381a5c6e7b02a2f69f7795bea88fa82
roger <1664465219@qq.com>
2022-01-10 22:33:11 +0800

    添加背景音乐

--- playAudio.js
+++ playAudio.js
@@ -1,5 +1,5 @@
 var myAudioElement = document.getElementById('audio');  
-myAudioElement.addEventListener("canplaythrough", event => {
+myAudioElement.addEventListener("canplay", event => {
   /* the audio is now playable; play it if permissions allow */
   myAudioElement.play();
 });
